1fd51086359e83d2828ae502b46c30fae93095de
Mushegh Zakaryan <mushzak@mail.ru>
2021-05-22 16:20:31 +0400

    create notes component with form validations UI

---
+++ src/app/home-page/components/create-note/create-note.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-create-note',
+  templateUrl: './create-note.component.html',
+  styleUrls: ['./create-note.component.scss']
+})
+export class CreateNoteComponent implements OnInit {
+  createForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  /**
+   * Initializes createForm property with the corresponding fields (title,description)
+   */
+  initializeForm(): void {
+    this.createForm = this.fb.group({
+      title: ['', [Validators.required, Validators.maxLength(255)]],
+      description: ['', Validators.required],
+    });
+  }
+
+  get title(): AbstractControl {
+    return this.createForm.get('title');
+  }
+
+  get description(): AbstractControl {
+    return this.createForm.get('description');
+  }
+
+  submitCreateForm(): void {
+    if (this.createForm.invalid) {
+      this.createForm.markAllAsTouched();
+    } else {
+      console.log(this.createForm.value);
+    }
+  }
+}
--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -3,16 +3,19 @@ import {CommonModule} from '@angular/common';
 import {HomePageComponent} from './components/home-page.component';
 import {HomePageRoutingModule} from './home-page-routing.module';
 import {SearchComponent} from './components/search/search.component';
-
+import { CreateNoteComponent } from './components/create-note/create-note.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     HomePageComponent,
     SearchComponent,
+    CreateNoteComponent,
   ],
   imports: [
     CommonModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class HomePageModule {
