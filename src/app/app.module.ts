e8113dd69bd6f3d07c92767f9420b7427c0dcb9f
Mushegh Zakaryan <mushzak@mail.ru>
2021-05-22 11:23:39 +0400

    created shared layout module + header and footer skeleton and added in app html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {LayoutModule} from './shared/modules/layout/layout.module';
+import {LoadingModule} from './shared/modules/loading/loading.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LayoutModule,
+    LoadingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
